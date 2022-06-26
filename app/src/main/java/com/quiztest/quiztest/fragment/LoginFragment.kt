f8b990daa9312596f015225043eed21ef3d24964
I'm Hung <znkcom89@gmail.com>
2022-06-26 23:43:17 +0700

    fragment login(fixed)2

--- app/src/main/java/com/quiztest/quiztest/fragment/LoginFragment.kt
+++ app/src/main/java/com/quiztest/quiztest/fragment/LoginFragment.kt
@@ -5,7 +5,6 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.quiztest.quiztest.R
 import com.quiztest.quiztest.databinding.FragmentLoginBinding
 import com.quiztest.quiztest.utils.Utils
 
