ff5a4ab3f263b50b9af9f7afc921147f195c8025
I'm Hung <znkcom89@gmail.com>
2022-06-26 21:01:52 +0700

    Revert "Merge branch 'MaiHung' into develop"
    This reverts commit 8e833339b227ee63b95d1f6d3036d7f7a906a440, reversing
    changes made to c78d5b6cab82a1af6bf903c86e281eddbc443c6a.

---
+++ app/src/main/java/com/quiztest/quiztest/ui/Login/LoginActivity.java
@@ -0,0 +1,16 @@
+package com.quiztest.quiztest.ui.Login;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.quiztest.quiztest.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
