49f98c5b072435feebc833b5719418ee992633ba
I'm Hung <znkcom89@gmail.com>
2022-06-26 23:50:50 +0700

    fragment login(fixed)2

--- app/src/main/java/com/quiztest/quiztest/fragment/LoginFragment.kt
+++ app/src/main/java/com/quiztest/quiztest/fragment/LoginFragment.kt
@@ -5,11 +5,13 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.quiztest.quiztest.R
+import com.quiztest.quiztest.base.BaseFragment
 import com.quiztest.quiztest.databinding.FragmentLoginBinding
 import com.quiztest.quiztest.utils.Utils
 
 
-class LoginFragment : Fragment() {
+class LoginFragment : BaseFragment() {
     private lateinit var binding: FragmentLoginBinding
     override fun onCreateView(
             inflater: LayoutInflater, container: ViewGroup?,
@@ -20,10 +22,18 @@ class LoginFragment : Fragment() {
         return binding.root
     }
 
-    fun initView() {
+    override fun getLayoutId(): Int {
+        return R.layout.fragment_login
+    }
+
+    override fun initView() {
         val height = Utils.getHeight(activity) * 240 / 800
         var layoutParams: ViewGroup.LayoutParams = ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, height)
         binding.imvThum.layoutParams = layoutParams
+    }
+
+    override fun initData() {
+
     }
 
 
