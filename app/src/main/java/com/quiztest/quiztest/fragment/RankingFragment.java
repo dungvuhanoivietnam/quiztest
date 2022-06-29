658ad24fd507bbcbc641a93a2dd3b3d89ead2f58
kiendttp <hoangns.merryblue@gmail.com>
2022-06-29 21:46:54 +0700

    fake data list ranking

--- app/src/main/java/com/quiztest/quiztest/fragment/RankingFragment.java
+++ app/src/main/java/com/quiztest/quiztest/fragment/RankingFragment.java
@@ -5,20 +5,30 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.core.content.ContextCompat;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.quiztest.quiztest.R;
+import com.quiztest.quiztest.adapter.RankingAdapter;
 import com.quiztest.quiztest.base.BaseFragment;
 import com.quiztest.quiztest.custom.ExtTextView;
+import com.quiztest.quiztest.model.RankerResponse;
 
-public class RankingFragment extends BaseFragment implements View.OnClickListener {
+import java.util.ArrayList;
+
+public class RankingFragment extends BaseFragment implements View.OnClickListener, RankingAdapter.ItemClickListener {
 
     public static final String TAG = RankingFragment.class.getSimpleName();
 
+    private RankingAdapter rankingAdapter;
+    private RecyclerView recyclerViewRanking;
+
     private ExtTextView extWeeklyRanking, extOverallRating;
+
     private final int ButtonWeeklyRanking = 1;
     private final int ButtonOverallRating = 2;
 
@@ -37,12 +47,29 @@ public class RankingFragment extends BaseFragment implements View.OnClickListene
     protected void initView(View v) {
         extWeeklyRanking = v.findViewById(R.id.ext_weekly_ranking);
         extOverallRating = v.findViewById(R.id.ext_overall_rating);
+        recyclerViewRanking = v.findViewById(R.id.rcv_ranking);
     }
 
     @Override
     protected void initData() {
         extWeeklyRanking.setOnClickListener(this);
         extOverallRating.setOnClickListener(this);
+
+        rankingAdapter = new RankingAdapter(getContext());
+        rankingAdapter.setListData(fakeDataRank());
+        rankingAdapter.setItemClickListener(this);
+        recyclerViewRanking.setAdapter(rankingAdapter);
+    }
+
+    private ArrayList<RankerResponse> fakeDataRank() {
+        ArrayList<RankerResponse> list = new ArrayList<>();
+        list.add(new RankerResponse("Alexpham", 1034));
+        list.add(new RankerResponse("Wade Warren", 1034));
+        list.add(new RankerResponse("Brooklyn Simmons", 1034));
+        list.add(new RankerResponse("Jenny Wilson", 1034));
+        list.add(new RankerResponse("Bessie Cooper", 1034));
+        list.add(new RankerResponse("Ralph Edwards", 1034));
+        return list;
     }
 
     @Override
@@ -77,5 +104,15 @@ public class RankingFragment extends BaseFragment implements View.OnClickListene
                 extTextView.setTextColor(getResources().getColor(R.color.color_8D959D));
             }
         }
+    }
+
+    @Override
+    public void onItemClickListener(int position) {
+        Toast.makeText(getContext(),position+"",Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onItemLongClickListener(int position) {
+
     }
 }
---
+++ app/src/main/java/com/quiztest/quiztest/model/RankerResponse.java
@@ -0,0 +1,27 @@
+package com.quiztest.quiztest.model;
+
+public class RankerResponse {
+    private String name;
+    private int score;
+
+    public RankerResponse(String name, int score) {
+        this.name = name;
+        this.score = score;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+}
